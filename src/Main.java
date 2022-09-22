6f16a60219ca5f139ba0972a832b7a32369f8dae
Дмитрий <dmitrykornev.64@gmail.com>
2022-09-22 18:31:38 +0400

    ДЗ 2. Циклы. Часть 1

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,30 @@ public class Main {
         for (int i = 10; i >= -10; i--){
             System.out.println(i);
         }
+
+
+
+
+        // Домашнее задание - 2.
+
+        // Задание - 1.
+        System.out.println("Задание - 1.");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+
+        // Задание - 2.
+        System.out.println("Задание - 2.");
+        for (int i = 7; i <= 98; i = i + 7){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Задание - 3.
+        System.out.println("Задание - 3.");
+        for (int i = 1; i <= 512; i = i * 2){
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
